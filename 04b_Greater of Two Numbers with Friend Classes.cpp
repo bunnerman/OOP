be0e6c12e243eaebbfc5bb7c44af5fc0f7539de3
pb4949 <parnilrbalawade@gmail.com>
2025-08-12 19:04:23 +0530

    Fixed 4.2 to actually use friend function

--- 04b_Greater of Two Numbers with Friend Classes.cpp
+++ 04b_Greater of Two Numbers with Friend Classes.cpp
@@ -2,51 +2,42 @@
 #include <string>
 using namespace std;
 
-class first
+class second;
+class first 
 {
-private: 
+private:
     int num;
 public:
-    void getdata()
-    {
+    void getdata() {
         cout << "Enter number 1: ";
         cin >> num;
     }
-    int returndata()
-    {
-        return num;
-    }
+    friend int func(first &obj1, second &obj2);
 };
-class second
+
+class second 
 {
-private: 
+private:
     int num;
 public:
-    void getdata()
-    {
+    void getdata() {
         cout << "Enter number 2: ";
         cin >> num;
     }
-    int returndata()
-    {
-        return num;
-    }
+    friend int func(first &obj1, second &obj2);
 };
 
-int func(first &obj1, second &obj2)
+int func(first &obj1, second &obj2) 
 {
-    int n1, n2;
-    n1 = obj1.returndata();
-    n2 = obj2.returndata();
-    if (n1 == n2)
+    if (obj1.num == obj2.num)
         return 0;
-    else if (n1 > n2)
+    else if (obj1.num > obj2.num)
         return 1;
     else
         return 2;
 }
 
-int main()
+int main() 
 {
     first obj1;
     second obj2;
@@ -55,6 +46,7 @@ int main()
     obj2.getdata();
 
     int result = func(obj1, obj2);
+
     if (result == 0)
         cout << "Both numbers are equal";
     else if (result == 1)
@@ -63,6 +55,6 @@ int main()
         cout << "Number 2 is greater";
     else
         cout << "Error, wrong return value returned";
-        
+
     return 0;
 }
