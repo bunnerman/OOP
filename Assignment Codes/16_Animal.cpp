bd414dc840236f644f595dfb3bb5e011d7d33f2b
pb4949 <parnilrbalawade@gmail.com>
2025-09-18 20:00:49 +0530

    Create 16_Animal.cpp

---
+++ Assignment Codes/16_Animal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+    public:
+    Animal()
+    {
+        cout << "Animal Created :)\n";
+    }
+    ~Animal()
+    {
+        cout << "Animal Destroyed :(\n";
+    }
+};
+
+int main()
+{
+    int n;
+    {
+        Animal pigeon;
+        cout << "Continuing by entering any integer will DESTROY animal as it goes out of scope: ";
+        cin >> n;
+    }
+    // Out of Scope of local block
+}
