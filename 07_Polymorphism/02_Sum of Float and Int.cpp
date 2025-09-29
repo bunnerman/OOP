760e344d3c9830531d54f7460ae12928b6dd4abd
pb4949 <parnilrbalawade@gmail.com>
2025-09-29 17:08:56 +0530

    made use function overloading

--- 07_Polymorphism/02_Sum of Float and Int.cpp
+++ 07_Polymorphism/02_Sum of Float and Int.cpp
@@ -2,24 +2,20 @@
 #include <string>
 using namespace std;
 
-class Summer
+void processData(int *aryInt, int size)
 {
-    public:
-    Summer(int *aryInt, int size)
-    {
-        int sum = 0;
-        for (int i = 0; i < size; i++)
-            sum += aryInt[i]; 
-        cout << "Sum (Integers) is " << sum << "\n";
-    }
-    Summer(float *aryFloat, int size)
-    {
-        float sum = 0;
-        for (int i = 0; i < size; i++)
-            sum += aryFloat[i];
-        cout << "Sum (Floating Point Integers) is " << sum << "\n";
-    }
-};
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += aryInt[i]; 
+    cout << "Sum (Integers) is " << sum << "\n";
+}
+void processData(float *aryFloat, int size)
+{
+    float sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += aryFloat[i];
+    cout << "Sum (Floating Point Integers) is " << sum << "\n";
+}
 
 int main()
 {
@@ -34,9 +30,9 @@ int main()
     cout << "Enter 10 floats: ";
     for (i = 0; i < 10; i++)
         cin >> aryFloat[i];
-
-    Summer obj1(aryInt, 5);
-    Summer obj2(aryFloat, 10);
+    
+    processData(aryInt, 5);
+    processData(aryFloat, 10);
 
     return 0;
 }
