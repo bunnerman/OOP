5bd8a7f7ae50fe42ba2838803985006c15b8badf
pb4949 <parnilrbalawade@gmail.com>
2025-09-29 22:32:43 +0530

    Create 08a_String Concatenation.cpp

---
+++ 08a_String Concatenation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Example 
+{
+    public:
+    string str;
+
+    Example(string s = "") 
+    {
+        str = s;
+    }
+
+    Example operator+(const Example& str2) 
+    {
+        return Example(str + str2.str); // just use std::string + 
+    }
+
+    void display() 
+    {
+        cout << str << endl;
+    }
+};
+
+int main() 
+{
+    string s1, s2;
+    cout << "Enter string 1: ";
+    cin >> s1;
+    cout << "Enter string 2: ";
+    cin >> s2;
+    Example obj1(s1);
+    Example obj2(s2);
+
+    Example obj3 = obj1 + obj2;
+    obj3.display();
+
+    return 0;
+}
